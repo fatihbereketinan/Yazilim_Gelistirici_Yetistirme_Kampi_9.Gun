0ed0a626b8e0aea717fbf22c63daf31f21d573af
fatihbereketinan <fatihbereketinan@gmail.com>
2022-09-16 11:15:49 +0300

    Yazilim_Gelistirici_Yetistirme_Kampi_9.Gun

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
+//@"Server=175.45.2.12" gerçek projelerde ip şeklinde olur.
+//gerçek projelerde Trusted_Connection=true yerine kullanıcı adı veya şifrede görebiliriz.
